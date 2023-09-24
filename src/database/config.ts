9ed80f7bcfde64cd75e2a327779b2766ba7cb3e5
B-Mahdj <bilal.mahdjoubi95@gmail.com>
2023-09-24 20:42:42 +0200

    Change DB config to have DigitalOcean Mongo database

--- src/database/config.ts
+++ src/database/config.ts
@@ -3,6 +3,25 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv');
 
 dotenv.config();
+const MongoDBDigitalOceanConfig = {
+    username: encodeURIComponent(process.env.MONGODB_DIGITALOCEAN_USERNAME),
+    password: encodeURIComponent(process.env.MONGODB_DIGITALOCEAN_PASSWORD)
+};
+
+const MongoDBUri = `mongodb+srv://${MongoDBDigitalOceanConfig.username}:${MongoDBDigitalOceanConfig.password}` + "@db-mongodb-lon1-lol-rush-0436540d.mongo.ondigitalocean.com/admin?tls=true&authSource=admin";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+export const client = new MongoClient(MongoDBUri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+/*
 
 const MongoDBAtlasConfig = {
     username: encodeURIComponent(process.env.MONGODB_ATLAS_USERNAME),
@@ -11,11 +30,12 @@ const MongoDBAtlasConfig = {
 
 const MongoDBAtlasURI = `mongodb+srv://${MongoDBAtlasConfig.username}:${MongoDBAtlasConfig.password}` + "@cluster0.gpbvoma.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-export const client = new MongoClient(MongoDBAtlasURI, {
+
+export const clientMongoDbAtlas = new MongoClient(MongoDBAtlasURI, {
     serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
     }
 });
+*/
