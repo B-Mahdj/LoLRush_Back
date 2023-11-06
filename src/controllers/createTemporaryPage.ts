c42379331c08c8814e44fc53e50204ce21b9a19a
Mahdjoubi Bilal <60183331+B-Mahdj@users.noreply.github.com>
2023-11-06 11:50:55 +0100

    Update createTemporaryPage.ts, don't push in global array empty player usernames

--- src/controllers/createTemporaryPage.ts
+++ src/controllers/createTemporaryPage.ts
@@ -17,12 +17,12 @@ export const createTemporaryPageController = async (req: express.Request, res: e
             // Take the values of player_1 up to player_8 if the values are not equals to undefined add them in an array called player_usernames
             if(player_1 !== undefined) player_usernames.push(player_1);
             if(player_2 !== undefined) player_usernames.push(player_2);
-            if(player_3 !== undefined) player_usernames.push(player_3);
-            if(player_4 !== undefined) player_usernames.push(player_4);
-            if(player_5 !== undefined) player_usernames.push(player_5);
-            if(player_6 !== undefined) player_usernames.push(player_6);
-            if(player_7 !== undefined) player_usernames.push(player_7);
-            if(player_8 !== undefined) player_usernames.push(player_8);
+            if(player_3 !== undefined || player_3 !== '') player_usernames.push(player_3);
+            if(player_4 !== undefined || player_4 !== '') player_usernames.push(player_4);
+            if(player_5 !== undefined || player_5 !== '') player_usernames.push(player_5);
+            if(player_6 !== undefined || player_6 !== '') player_usernames.push(player_6);
+            if(player_7 !== undefined || player_7 !== '') player_usernames.push(player_7);
+            if(player_8 !== undefined || player_8 !== '') player_usernames.push(player_8);
             // Call the createTemporaryPage function and pass the array of player_usernames, region and daysUntilExpiration 
             const result = await createChallenge(email, player_usernames, region, daysUntilExpiration);
             return res.status(200).json(result);
